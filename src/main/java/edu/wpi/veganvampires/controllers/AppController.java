d8fde00f6f2b60c24e0b1a40c4f8241ee2c7e01c
Tdonnelly20 <tate.donne@gmail.com>
2022-03-23 12:04:27 -0400

    Page Swap Functionality
    Signed-off-by: Tdonnelly20 <tate.donne@gmail.com>

--- src/main/java/edu/wpi/veganvampires/controllers/AppController.java
+++ src/main/java/edu/wpi/veganvampires/controllers/AppController.java
@@ -1,11 +1,37 @@
 package edu.wpi.veganvampires.controllers;
 
+import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class AppController {
 
+  private Stage stage;
+  private Scene scene;
+  private Parent root;
+
   @FXML
-  void sr1() {}
+  public void switchToDefault(ActionEvent event) throws IOException {
+    root = FXMLLoader.load(getClass().getClassLoader().getResource("FXML/app.fxml"));
+    stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  @FXML
+  public void switchToLabRequest(ActionEvent event) throws IOException {
+    root = FXMLLoader.load(getClass().getClassLoader().getResource("FXML/LabRequest.fxml"));
+    stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+  }
 
   @FXML
   void sr2() {}
