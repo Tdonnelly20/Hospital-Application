db37fddf0c9293c087da118bb93c9b0b2862ff01
Jakob <jsimmons01778@gmail.com>
2022-03-22 20:05:07 -0400

    Front End

--- src/main/java/edu/wpi/veganvampires/Main.java
+++ src/main/java/edu/wpi/veganvampires/Main.java
@@ -3,6 +3,7 @@ package edu.wpi.veganvampires;
 public class Main {
 
   public static void main(String[] args) {
+    System.out.println("This is front end.");
     App.launch(App.class, args);
   }
 }
