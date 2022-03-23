7d864f40b11c5ade791b1aae79983e53735334a5
Tdonnelly20 <tate.donne@gmail.com>
2022-03-23 19:12:45 -0400

    Derby Demo
    Signed-off-by: Tdonnelly20 <tate.donne@gmail.com>

--- src/main/java/edu/wpi/veganvampires/Main.java
+++ src/main/java/edu/wpi/veganvampires/Main.java
@@ -1,9 +1,42 @@
 package edu.wpi.veganvampires;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("This is back end");
-    App.launch(App.class, args);
+    System.out.println("-------Embedded Apache Derby Connection Testing --------");
+    try {
+      Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+    } catch (ClassNotFoundException e) {
+      System.out.println("Apache Derby Driver not found. Add the classpath to your module.");
+      System.out.println("For IntelliJ do the following:");
+      System.out.println("File | Project Structure, Modules, Dependency tab");
+      System.out.println("Add by clicking on the green plus icon on the right of the window");
+      System.out.println(
+          "Select JARs or directories. Go to the folder where the database JAR is located");
+      System.out.println("Click OK, now you can compile your program and run it.");
+      e.printStackTrace();
+      return;
+    }
+
+    System.out.println("Apache Derby driver registered!");
+    Connection connection = null;
+
+    try {
+      // substitute your database name for myDB
+      connection = DriverManager.getConnection("jdbc:derby:myDB;create=true");
+      Statement exampleStatement = connection.createStatement();
+      exampleStatement.execute("CREATE TABLE TEST(id int primary key)");
+      exampleStatement.execute("INSERT INTO TEST VALUES(2)");
+    } catch (SQLException e) {
+      System.out.println("Connection failed. Check output console.");
+      e.printStackTrace();
+      return;
+    }
+    System.out.println("Apache Derby connection established!");
   }
 }
