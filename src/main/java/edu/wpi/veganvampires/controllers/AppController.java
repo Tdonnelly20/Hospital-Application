521b1759fdde452d91a3f07b0ba9c76a074dea51
Jakob <jsimmons01778@gmail.com>
2022-03-23 11:15:19 -0400

    Service Request Homepage created, no functionality added yet

--- src/main/java/edu/wpi/veganvampires/controllers/AppController.java
+++ src/main/java/edu/wpi/veganvampires/controllers/AppController.java
@@ -1,3 +1,24 @@
 package edu.wpi.veganvampires.controllers;
 
-public class AppController {}
+import javafx.fxml.FXML;
+
+public class AppController {
+
+  @FXML
+  void sr1() {}
+
+  @FXML
+  void sr2() {}
+
+  @FXML
+  void sr3() {}
+
+  @FXML
+  void sr4() {}
+
+  @FXML
+  void sr5() {}
+
+  @FXML
+  void sr6() {}
+}
