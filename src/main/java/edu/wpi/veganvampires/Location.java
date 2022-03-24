141675c26319c90fca489a37acb32d62f70752a8
Tucker Raymond <teraymond@wpi.edu>
2022-03-23 21:13:00 -0400

    Added Location Java Class

---
+++ src/main/java/edu/wpi/veganvampires/Location.java
@@ -0,0 +1,36 @@
+package edu.wpi.veganvampires;
+
+public class Location {
+    String nodeID;
+    int xCoord;
+    int yCoord;
+    int floor;
+    String building;
+    String nodeType;
+    String longName;
+    String shortName;
+
+    public Location()
+    {
+        nodeID = null;
+        xCoord = 0;
+        yCoord = 0;
+        floor = 0;
+        building = null;
+        nodeType = null;
+        longName = null;
+        shortName = null;
+    }
+
+    public Location(String nodeID, int xCoord, int yCoord, int floor, String building, String nodeType, String longName, String shortName)
+    {
+        this.nodeID = nodeID;
+        this.xCoord = xCoord;
+        this.yCoord = yCoord;
+        this.floor = floor;
+        this.building = building;
+        this.nodeType = nodeType;
+        this.longName = longName;
+        this.shortName = shortName;
+    }
+}
