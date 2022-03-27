57c656d5ab7b6f1afe91124172abab3533230f69
mchrpt <mchrpt@yahoo.com>
2022-03-27 17:46:36 -0400

    Updated front end UI

--- src/main/java/edu/wpi/veganvampires/controllers/Controller.java
+++ src/main/java/edu/wpi/veganvampires/controllers/Controller.java
@@ -10,13 +10,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public abstract class Controller extends Application {
   private Parent root;
 
-  //Closes the program
+  // Closes the program
   @Override
   public void stop() {
     System.out.println("Shutting Down");
