b28501c47938bcfea2ce95258eec8b8c41adc1bf
Tdonnelly20 <tate.donne@gmail.com>
2022-03-28 20:21:57 -0400

    Fix

--- src/main/java/edu/wpi/veganvampires/Controllers/MedicineDeliveryController.java
+++ src/main/java/edu/wpi/veganvampires/Controllers/MedicineDeliveryController.java
@@ -107,19 +107,4 @@ public class MedicineDeliveryController extends Controller {
 
   @Override
   public void start(Stage primaryStage) throws Exception {}
-
-  /**
-   * Determines if a String is an integer or not
-   *
-   * @param input is a string
-   * @return true if the string is an integer, false if not
-   */
-  public boolean isInteger(String input) {
-    try {
-      Integer.parseInt(input);
-      return true;
-    } catch (NumberFormatException e) {
-      return false;
-    }
-  }
 }
