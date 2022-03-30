7d4439e73903c5eefb7e265b7ee11c7c0a9bd750
MottledPetrel <vzmemez@gmail.com>
2022-03-29 20:38:05 -0400

    SanitationRequests addition

--- src/main/java/edu/wpi/veganvampires/Controllers/Controller.java
+++ src/main/java/edu/wpi/veganvampires/Controllers/Controller.java
@@ -92,7 +92,7 @@ public abstract class Controller extends Application {
     root =
         FXMLLoader.load(
             Objects.requireNonNull(
-                getClass().getClassLoader().getResource("FXML/SanitationRequests.fxml")));
+                getClass().getClassLoader().getResource("FXML/SanitationRequest.fxml")));
     switchScene(event);
   }
 
--- src/main/java/edu/wpi/veganvampires/Controllers/SanitationRequestController.java
+++ src/main/java/edu/wpi/veganvampires/Controllers/SanitationRequestController.java
@@ -1,8 +1,32 @@
 package edu.wpi.veganvampires.Controllers;
 
+import com.jfoenix.controls.JFXComboBox;
+import edu.wpi.veganvampires.Dao.SanitationRequestDao;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class SanitationRequestController extends Controller {
   @Override
   public void start(Stage primaryStage) throws Exception {}
+
+  @FXML private TextField patientID;
+
+  @FXML private TextField firstName;
+  @FXML private TextField lastName;
+  /*
+    @FXML private TextField location;
+  */
+  @FXML private JFXComboBox<Object> sanitationDropDown;
+  @FXML private Button sendRequest;
+  @FXML private TextArea requestDetails;
+  @FXML private Label statusLabel;
+
+  private static SanitationRequestDao SanitationRequestDao = new SanitationRequestDao();
+
+  @FXML
+  public void checkValidation() throws Exception {}
 }
