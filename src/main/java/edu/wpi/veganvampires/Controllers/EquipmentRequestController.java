63bd2077dbc04c37dca974d4333e0027c834a645
Jakob Simmons <JSimmons01778@gmail.com>
2022-03-29 20:53:38 -0400

    table function on Equipment Request, Only database connection left

--- src/main/java/edu/wpi/veganvampires/Controllers/EquipmentRequestController.java
+++ src/main/java/edu/wpi/veganvampires/Controllers/EquipmentRequestController.java
@@ -2,20 +2,54 @@ package edu.wpi.veganvampires.Controllers;
 
 import com.jfoenix.controls.JFXComboBox;
 import edu.wpi.veganvampires.Dao.EquipmentDeliveryDao;
+import edu.wpi.veganvampires.Features.EquipmentDelivery;
+import java.util.ArrayList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.TreeItemPropertyValueFactory;
 import javafx.stage.Stage;
 
 public class EquipmentRequestController extends Controller {
+
+  @FXML private TreeTableView<EquipmentDelivery> equipmentRequestTable;
+  @FXML private TreeTableColumn<EquipmentDelivery, String> posCol;
+  @FXML private TreeTableColumn<EquipmentDelivery, String> equipCol;
+  @FXML private TreeTableColumn<EquipmentDelivery, Integer> quantCol;
+  @FXML private TreeTableColumn<EquipmentDelivery, String> notesCol;
+
   @FXML private TextField status;
   @FXML private TextField pos;
   @FXML private JFXComboBox<Object> dropDown;
   @FXML private TextField quant;
   @FXML private TextArea notes;
   @FXML private Button sendRequest;
+
   private static EquipmentDeliveryDao equipmentDeliveryDao = new EquipmentDeliveryDao();
+
+  @FXML
+  private void updateTreeTable() {
+    posCol.setCellValueFactory(new TreeItemPropertyValueFactory("location"));
+    equipCol.setCellValueFactory(new TreeItemPropertyValueFactory("equipment"));
+    quantCol.setCellValueFactory(new TreeItemPropertyValueFactory("quantity"));
+    notesCol.setCellValueFactory(new TreeItemPropertyValueFactory("notes"));
+
+    ArrayList<EquipmentDelivery> currEquipmentDeliveries =
+        (ArrayList<EquipmentDelivery>) equipmentDeliveryDao.getAllEquipmentDeliveries();
+
+    ArrayList<TreeItem> treeItems = new ArrayList<>();
+
+    if (!currEquipmentDeliveries.isEmpty()) {
+
+      for (EquipmentDelivery delivery : currEquipmentDeliveries) {
+        TreeItem<EquipmentDelivery> item = new TreeItem(delivery);
+        treeItems.add(item);
+      }
+      equipmentRequestTable.setShowRoot(false);
+      TreeItem root = new TreeItem(equipmentDeliveryDao.getAllEquipmentDeliveries().get(0));
+      equipmentRequestTable.setRoot(root);
+      root.getChildren().addAll(treeItems);
+    }
+  }
 
   @FXML
   private void resetForm() {
