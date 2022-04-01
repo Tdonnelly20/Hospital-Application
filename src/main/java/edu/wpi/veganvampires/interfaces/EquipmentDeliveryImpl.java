6b67a3c8469d8344f3510f81b81f0a1bba17049d
mchrpt <mchrpt@yahoo.com>
2022-04-01 13:09:55 -0400

    attempting to make merge easier

---
+++ src/main/java/edu/wpi/veganvampires/interfaces/EquipmentDeliveryImpl.java
@@ -0,0 +1,13 @@
+package edu.wpi.veganvampires.Interfaces;
+
+import edu.wpi.veganvampires.EquipmentDelivery;
+import java.util.List;
+
+public interface EquipmentDeliveryImpl {
+
+  List<EquipmentDelivery> getAllEquipmentDeliveries();
+
+  void addEquipmentDelivery(String location, String equipment, String notes, int quantity);
+
+  void removeEquipmentDelivery(String equipment);
+}
