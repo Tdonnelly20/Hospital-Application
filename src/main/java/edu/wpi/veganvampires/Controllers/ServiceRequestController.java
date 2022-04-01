4de265390ec2717c2f28cc4f13939584a1d928a8
Tdonnelly20 <tate.donne@gmail.com>
2022-04-01 10:00:57 -0400

    Don't worry about it

--- src/main/java/edu/wpi/veganvampires/Controllers/ServiceRequestController.java
+++ src/main/java/edu/wpi/veganvampires/Controllers/ServiceRequestController.java
@@ -4,20 +4,6 @@ import java.awt.*;
 import javafx.stage.Stage;
 
 public class ServiceRequestController extends Controller {
-
-  // Singleton design pattern
-  private static final ServiceRequestController controller = new ServiceRequestController();
-
-  private ServiceRequestController() {}
-
-  private static class SingletonHelper {
-    private static final ServiceRequestController controller = new ServiceRequestController();
-  }
-
-  public static ServiceRequestController getController() {
-    return ServiceRequestController.SingletonHelper.controller;
-  }
-
   @Override
   public void start(Stage primaryStage) {}
 }
