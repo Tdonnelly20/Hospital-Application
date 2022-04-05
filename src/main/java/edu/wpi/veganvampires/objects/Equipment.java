e06378d143d5ca4ad3e6d5b89942a422c4a1889c
Tdonnelly20 <tate.donne@gmail.com>
2022-04-04 20:40:04 -0400

    equipment

--- src/main/java/edu/wpi/veganvampires/objects/Equipment.java
+++ src/main/java/edu/wpi/veganvampires/objects/Equipment.java
@@ -1,14 +1,18 @@
 package edu.wpi.veganvampires.objects;
 
 public class Equipment {
-  private Location location;
+  private int ID;
+  private double x;
+  private double y;
   private String name;
   private String description;
   private int count;
   private boolean isDirty;
 
-  public Equipment(Location location, String name, String description, int count, Boolean isDirty) {
-    this.location = location;
+  public Equipment(int ID, double x, double y, String name, String description, int count, Boolean isDirty) {
+    this.ID=ID;
+    this.x = x;
+    this.y = y;
     this.name = name;
     this.description = description;
     this.count = count;
