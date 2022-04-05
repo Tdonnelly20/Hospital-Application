2ce84ced1a92937e8a4be22700b8a091e130d835
mchrpt <mchrpt@yahoo.com>
2022-04-05 16:46:51 -0400

    Fixed LaundryRequestController

--- src/main/java/edu/wpi/veganvampires/controllers/LaundryRequestController.java
+++ src/main/java/edu/wpi/veganvampires/controllers/LaundryRequestController.java
@@ -1,10 +1,14 @@
 package edu.wpi.veganvampires.controllers;
 
+import com.jfoenix.controls.JFXComboBox;
 import edu.wpi.veganvampires.dao.LaundryRequestDao;
+import java.awt.*;
 import java.sql.SQLException;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -16,7 +20,8 @@ public class LaundryRequestController extends Controller {
   @FXML private TextField firstName;
   @FXML private TextField lastName;
   @FXML private TextField roomNumber;
-  @FXML private TextField details;
+  @FXML private TextArea details;
+  @FXML private JFXComboBox statusDropDown;
   @FXML private Button sendRequest;
 
   // TODO
@@ -28,13 +33,16 @@ public class LaundryRequestController extends Controller {
 
   @FXML
   private void resetForm() {
-    Status.setText("Status: Blank");
     userID.setText("");
     patientID.setText("");
     firstName.setText("");
     lastName.setText("");
     roomNumber.setText("");
+    details.setText("");
+    statusDropDown.setValue("Status");
+    Status.setText("Status: Blank");
     sendRequest.setDisable(true);
+    System.out.println("reset form");
   }
 
   // Checks to see if the user can submit info
@@ -63,6 +71,7 @@ public class LaundryRequestController extends Controller {
     }
   }
 
+  @FXML
   private void sendRequest() throws SQLException {
     laundryRequestDao.addLaundryRequest(
         userID.getText(),
@@ -76,4 +85,19 @@ public class LaundryRequestController extends Controller {
 
   @Override
   public void start(Stage primaryStage) {}
+
+  public void updateTreeTable(Event event) {}
+
+  // used to get coordinates after clicking map
+  @FXML private TextArea coordinates;
+  private Point point = new Point();
+  private int xCoord, yCoord;
+
+  @FXML
+  private void mapCoordTracker() {
+    point = MouseInfo.getPointerInfo().getLocation();
+    xCoord = point.x - 712;
+    yCoord = point.y - 230;
+    coordinates.setText("X: " + xCoord + " Y: " + yCoord);
+  }
 }
