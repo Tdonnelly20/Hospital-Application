c8d9dc0c1c9a8b7fcf025b5b84bff54786fcd25c
Jakob Simmons <JSimmons01778@gmail.com>
2022-04-05 15:56:18 -0400

    making equipment request table work

--- src/main/java/edu/wpi/veganvampires/objects/EquipmentDelivery.java
+++ src/main/java/edu/wpi/veganvampires/objects/EquipmentDelivery.java
@@ -3,6 +3,10 @@ package edu.wpi.veganvampires.objects;
 import edu.wpi.veganvampires.main.Vdb;
 
 public class EquipmentDelivery extends ServiceRequest {
+  int employeeID;
+  int patientID;
+  String patientFirstName;
+  String patientLastName;
   private final String equipment, notes;
   private final int quantity;
 
@@ -10,12 +14,32 @@ public class EquipmentDelivery extends ServiceRequest {
       String nodeID, int userID, String equipment, String notes, int quantity, String status) {
     this.location = Vdb.locationDao.getLocation(nodeID);
     this.hospitalEmployee = new HospitalEmployee(userID);
-    this.patient = null;
+    this.patient = new Patient(123, "James", "Jameson");
     this.equipment = equipment;
     this.notes = notes;
     this.desc = "Equipment Delivery (" + equipment + ")";
     this.status = status;
     this.quantity = quantity;
+    employeeID = hospitalEmployee.getHospitalID();
+    patientID = patient.getPatientID();
+    patientFirstName = patient.getFirstName();
+    patientLastName = patient.getLastName();
+  }
+
+  public int getEmployeeID() {
+    return employeeID;
+  }
+
+  public int getPatientID() {
+    return patientID;
+  }
+
+  public String getPatientFirstName() {
+    return patientFirstName;
+  }
+
+  public String getPatientLastName() {
+    return patientLastName;
   }
 
   public Location getLocation() {
