bb5bb4b8646cef8a699d85b52f0e7d89a74f83a7
MottledPetrel <vzmemez@gmail.com>
2022-04-10 16:12:42 -0400

    SanitationRequests addition

--- src/main/java/edu/wpi/cs3733/d22/teamV/controllers/LoginController.java
+++ src/main/java/edu/wpi/cs3733/d22/teamV/controllers/LoginController.java
@@ -1,6 +1,9 @@
 package edu.wpi.cs3733.d22.teamV.controllers;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -25,15 +28,21 @@ public class LoginController extends Controller {
     checkLogin(event);
   }
 
+  private Map<String, String> UserTable = Map.of("admin", "admin");
+
   private void checkLogin(ActionEvent event) throws IOException {
-    if (username.getText().toString().equals("admin")
-        && password.getText().toString().equals("admin")) {
-      wrongLogIn.setText("Success!");
-      switchToHome(event);
-    } else if (username.getText().isEmpty() && password.getText().isEmpty()) {
-      wrongLogIn.setText("Please enter your data.");
-    } else {
-      wrongLogIn.setText("Wrong username or password!");
+
+
+    for (Map.Entry<String, String> set : UserTable.entrySet()) {
+      if (username.getText().toString().equals(set.getKey())
+          && password.getText().toString().equals(set.getValue())) {
+        wrongLogIn.setText("Success!");
+        switchToHome(event);
+      } else if (username.getText().isEmpty() && password.getText().isEmpty()) {
+        wrongLogIn.setText("Please enter your data.");
+      } else {
+        wrongLogIn.setText("Wrong username or password!");
+      }
     }
   }
 }
