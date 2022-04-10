b474ffc12373a99425190deafc320ea535a05245
pvanham <prvanham@comcast.net>
2022-04-10 19:33:51 -0400

    better looking login page

--- src/main/java/edu/wpi/cs3733/d22/teamV/controllers/LoginController.java
+++ src/main/java/edu/wpi/cs3733/d22/teamV/controllers/LoginController.java
@@ -1,13 +1,14 @@
 package edu.wpi.cs3733.d22.teamV.controllers;
 
 import java.io.IOException;
-import java.util.Map;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class LoginController extends Controller {
@@ -15,31 +16,36 @@ public class LoginController extends Controller {
   public LoginController() {}
 
   @Override
-  public void start(Stage primaryStage) throws Exception {}
+  public void start(Stage primaryStage) throws Exception {
+    iv.fitWidthProperty().bind(pane.widthProperty());
+    iv.fitHeightProperty().bind(pane.heightProperty());
+  }
 
   @FXML private Button button;
   @FXML private Label wrongLogIn;
   @FXML private TextField username;
   @FXML private PasswordField password;
+  @FXML private ImageView iv;
+  @FXML private Pane pane;
 
-  public void userLogIn(ActionEvent event) throws IOException {
+  @FXML
+  public void userLogin(ActionEvent event) throws IOException {
     checkLogin(event);
   }
 
-  private Map<String, String> UserTable = Map.of("admin", "admin", "staff", "staff");
+  // private Map<String, String> UserTable = Map.of("admin", "admin", "staff", "staff");
 
   private void checkLogin(ActionEvent event) throws IOException {
 
-    for (Map.Entry<String, String> set : UserTable.entrySet()) {
-      if (username.getText().toString().equals(set.getKey())
-          && password.getText().toString().equals(set.getValue())) {
-        wrongLogIn.setText("Success!");
-        switchToHome(event);
-      } else if (username.getText().isEmpty() && password.getText().isEmpty()) {
-        wrongLogIn.setText("Please enter your data.");
-      } else {
-        wrongLogIn.setText("Wrong username or password!");
-      }
+    if (username.getText().toString().equals("admin")
+        && password.getText().toString().equals("admin")) {
+      // wrongLogIn.setText("Success!");
+      System.out.println("Success!");
+      switchToHome(event);
+    } else if (username.getText().isEmpty() && password.getText().isEmpty()) {
+      wrongLogIn.setText("Please enter your data.");
+    } else {
+      wrongLogIn.setText("Wrong username or password!");
     }
   }
 }
