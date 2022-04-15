aa91b798872b87a857620797f591dfdd1975c0cf
mchrpt <mchrpt@yahoo.com>
2022-04-15 16:51:29 -0400

    Added some of the basic Observer interface clasess/functionality

---
+++ src/main/java/edu/wpi/cs3733/d22/teamV/observer/Subject.java
@@ -0,0 +1,19 @@
+package edu.wpi.cs3733.d22.teamV.observer;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public abstract class Subject {
+    ArrayList<Observer> observers = new ArrayList<>();
+
+    public void updateAllObservers() throws SQLException, IOException {
+        for(Observer observer : observers){
+            observer.update(this);
+        }
+    }
+
+    public void attach(Observer observer){
+        observers.add(observer);
+    }
+}
