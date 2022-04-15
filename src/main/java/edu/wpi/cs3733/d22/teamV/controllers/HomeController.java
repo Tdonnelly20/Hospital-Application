b77e0dcdb82bfea0fc8f210f094b039d558de8a5
Jakob Simmons <JSimmons01778@gmail.com>
2022-04-15 15:42:28 -0400

    development merge and small fix

--- src/main/java/edu/wpi/cs3733/d22/teamV/controllers/HomeController.java
+++ src/main/java/edu/wpi/cs3733/d22/teamV/controllers/HomeController.java
@@ -1,21 +1,18 @@
 package edu.wpi.cs3733.d22.teamV.controllers;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class HomeController extends Controller {
 
   @Override
   public void start(Stage primaryStage) throws Exception {}
 
-  @FXML
-  private Text currTime;
+  @FXML private Text currTime;
 
   @FXML
   protected void switchToPatientDB(ActionEvent event) {
@@ -37,9 +34,9 @@ public class HomeController extends Controller {
 
   @Override
   public void init() {
-    while(true){
+    while (true) {
       currTime.setText(getDateAndTime());
-      if(System.currentTimeMillis() >= 2147483647) break;
+      if (System.currentTimeMillis() >= 2147483647) break;
     }
   }
 }
