184abbebfdc09bea0ea72620a4d246e96ff58d3b
pvanham <prvanham@comcast.net>
2022-04-15 12:31:42 -0400

    map dashboard header

--- src/main/java/edu/wpi/cs3733/d22/teamV/controllers/MapDashboardController.java
+++ src/main/java/edu/wpi/cs3733/d22/teamV/controllers/MapDashboardController.java
@@ -5,23 +5,21 @@ import javafx.scene.control.TreeTableView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
-
 import javax.swing.text.html.ImageView;
 
 public class MapDashboardController {
 
-    private @FXML HBox hBox;
-    private @FXML VBox leftVBox;
-    private @FXML Pane sideViewPane;
-    private @FXML ImageView sideViewImage;
-    private @FXML VBox centerVBox;
-    private @FXML TreeTableView infoTable;
-    private @FXML TreeTableView countTable;
-    private @FXML VBox rightVBox;
-    private @FXML Pane mapPane;
-    private @FXML ImageView mapImage;
-    private @FXML TreeTableView alertTable;
-    //still need other HBOX / buttons
-
+  private @FXML HBox hBox;
+  private @FXML VBox leftVBox;
+  private @FXML Pane sideViewPane;
+  private @FXML ImageView sideViewImage;
+  private @FXML VBox centerVBox;
+  private @FXML TreeTableView infoTable;
+  private @FXML TreeTableView countTable;
+  private @FXML VBox rightVBox;
+  private @FXML Pane mapPane;
+  private @FXML ImageView mapImage;
+  private @FXML TreeTableView alertTable;
+  // still need other HBOX / buttons
 
 }
