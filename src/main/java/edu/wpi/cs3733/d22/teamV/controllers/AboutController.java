29f2ff33336f5eb982b70755e7937cc8bd8ff192
Jolene Pern <jpern@wpi.edu>
2022-04-18 17:40:53 -0400

    about page - still in progress

---
+++ src/main/java/edu/wpi/cs3733/d22/teamV/controllers/AboutController.java
@@ -0,0 +1,10 @@
+package edu.wpi.cs3733.d22.teamV.controllers;
+
+import javafx.stage.Stage;
+
+public class AboutController extends Controller{
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
