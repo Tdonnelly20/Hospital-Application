104a4eff4c553d62077005075b1c667af10b971b
mchrpt <mchrpt@yahoo.com>
2022-04-19 15:03:54 -0400

    Fixed concurrent exception

--- src/main/java/edu/wpi/cs3733/d22/teamV/observer/DirectionalAssoc.java
+++ src/main/java/edu/wpi/cs3733/d22/teamV/observer/DirectionalAssoc.java
@@ -6,9 +6,9 @@ public abstract class DirectionalAssoc {
   ArrayList<DirectionalAssoc> observers = new ArrayList<>();
 
   public void updateAllObservers() {
-    System.out.println("Running update! for " + observers.size());
-    for (DirectionalAssoc observer : observers) {
-      // observer.update(this);
+    for (int i = 0; i < observers.size(); i++) {
+      observers.get(i).update(this);
+      DirectionalAssoc.release(observers.get(i), this);
     }
   }
 
@@ -27,9 +27,9 @@ public abstract class DirectionalAssoc {
 
   // remove all associations from this observer
   public void releaseAll() {
-    for (DirectionalAssoc observer : observers) {
-      DirectionalAssoc.release(observer, this);
-      observer.update(this);
+    for (int i = 0; i < observers.size(); i++) {
+      observers.get(i).update(this);
+      DirectionalAssoc.release(observers.get(i), this);
     }
   }
 
