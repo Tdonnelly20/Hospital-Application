43b96119878a578736441edfe014a7e5c01e9f3d
anegronparra <101909034+anegronparra@users.noreply.github.com>
2022-04-24 16:10:54 -0400

    updated alertSixBeds, where its called and called RequestSystem

--- src/main/java/edu/wpi/cs3733/d22/teamV/map/EquipmentIcon.java
+++ src/main/java/edu/wpi/cs3733/d22/teamV/map/EquipmentIcon.java
@@ -7,6 +7,8 @@ import edu.wpi.cs3733.d22.teamV.main.RequestSystem;
 import edu.wpi.cs3733.d22.teamV.objects.Equipment;
 import edu.wpi.cs3733.d22.teamV.objects.Location;
 import java.util.ArrayList;
+
+import edu.wpi.cs3733.d22.teamV.servicerequests.EquipmentDelivery;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.*;
@@ -126,7 +128,7 @@ public class EquipmentIcon extends Icon {
       equipmentList.add(0, equipment);
     }
     setImage();
-    alertSixBeds(equipment);
+    alertSixBeds(equipment, true);
     pumpAlert();
   }
 
@@ -134,7 +136,7 @@ public class EquipmentIcon extends Icon {
   public void removeEquipment(Equipment equipment) {
     equipmentList.remove(equipment);
     RequestSystem.getSystem().removeEquipment(equipment);
-    alertSixBeds(equipment);
+    alertSixBeds(equipment, false);
     pumpAlert();
     PopupController.getController().closePopUp();
   }
@@ -190,13 +192,20 @@ public class EquipmentIcon extends Icon {
     }
   }
 
-  // finds beds that are dirty in the same place. When counter > 5, it returns true.
-  public void alertSixBeds(Equipment e) {
-    if (e.getIsDirty() == true && e.getName() == "Bed") {
-      dirtyBeds += 1; }
-    if (dirtyBeds > 5) {
-      //TODO do service request
+  //checks if isAdding is true, if so finds beds that are dirty in the same place.
+  //when counter > 5, dirtyBeds increases by 1 and RequestSystem is called (EquipmentDelivery).
+  //else, dirtyBeds decreases by 1.
+  public void alertSixBeds(Equipment e, boolean isAdding) {
+    if (isAdding) {
+      if (e.getIsDirty() && e.getName() == "Bed") {
+        dirtyBeds += 1;
+      }
+      if (dirtyBeds > 5) {
+        EquipmentDelivery request = new EquipmentDelivery(-1, -1, "OR", e.getID(), e.getID(), 1, "Not Started");
+        RequestSystem.getSystem().addServiceRequest(request);
+      }
     }
+    else { dirtyBeds--; }
   }
 
 
